202fef96e20007e470b2ee93181ea15efb3a3f9a
C.J. Christian <118404402+CJbricks@users.noreply.github.com>
2023-10-11 20:34:00 -0500

    Meta data

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
         <title>Bricks Design House</title>
         <meta name="description" content="Modern Web Development and Design | Build Better Brick By Brick." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:type" content="website" />
         <meta property="og:title" content= "Bricks Design House"/>
         <meta property="og:description" content="Modern Web Development, Web Design, and Graphic Design." />
         <meta property="og:image" content="https://live.staticflickr.com/65535/53252114650_f0ae94d8d3_m.jpg" />
